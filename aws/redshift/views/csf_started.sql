e239d61690e2d0a1fb64fbc8aa167034ea1e7dd7
bencodeorg <ben@code.org>
2018-06-01 17:05:43 -0700

    Add school year to CSF started view [ci skip]

--- aws/redshift/views/csf_started.sql
+++ aws/redshift/views/csf_started.sql
@@ -3,11 +3,13 @@ select
   us.script_id, 
   sc.name script_name, 
   us.user_id, 
-  started_at,
-  last_progress_at
+  us.started_at,
+  us.last_progress_at,
+  sy.school_year
 from dashboard_production.user_scripts us
   join dashboard_production.scripts sc on sc.id = us.script_id
   join dashboard_production.users u on u.id = us.user_id and u.user_type = 'student'
+  join analysis.school_years sy on us.started_at between sy.started_at and sy.ended_at
 where sc.name in 
 (
   '20-hour',
