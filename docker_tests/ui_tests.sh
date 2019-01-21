dd4185ae6e62d155eb191564dde35284a424b863
EC2 Default User <ec2-user@ip-172-30-235-161.us-west-2.compute.internal>
2019-01-21 21:37:50 +0000

    Update networking config in compose for ui tests redis

--- docker_tests/ui_tests.sh
+++ docker_tests/ui_tests.sh
@@ -35,27 +35,17 @@ sudo chmod -R 0777 dashboard/db \
 sudo chown $user /home/circleci/project/vendor/bundle \
 	apps/node_modules
 
-# TODO: recreate git checkout logic more accurately; UI test seeding relies on git state being correct
-
-# name: taking these out of dockerfile to see if that fixes build issues
-#sudo mv /usr/bin/parallel /usr/bin/gnu_parallel
-#sudo apt-get update
-#sudo apt-get install -y libicu-dev enscript moreutils pdftk libmysqlclient-dev libsqlite3-dev
-#wget https://github.com/htacg/tidy-html5/releases/download/5.4.0/tidy-5.4.0-64bit.deb && sudo dpkg -i tidy-5.4.0-64bit.deb
-#sudo mv /usr/bin/gnu_parallel /usr/bin/parallel
-
 # start mysql
 sudo service mysql start && mysql -V
 
 # name: install dependencies
 bundle check --path=/home/circleci/project/vendor/bundle || bundle install --deployment --path=/home/circleci/project/vendor/bundle --jobs=4 --retry=3 --without ''
 
-# name: set yarn version
-#sudo apt-get install yarn=1.6.0-1
-
 # set up locals.yml
 # Need to actually write all the commented out lines also
 echo "
+netsim_redis_groups:
+- master: redis://ui-tests-redis:6379
 bundler_use_sudo: false
 properties_encryption_key: $PROPERTIES_ENCRYPTION_KEY
 applitools_eyes_api_key: $APPLITOOLS_KEY
