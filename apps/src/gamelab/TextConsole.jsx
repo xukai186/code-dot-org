7992b6305cfebeeedc2b80831156a7fcb080a030
Jessica Kulwik <jessica@code.org>
2019-04-18 01:26:56 +0000

    Text console opens and closes with button click now

--- apps/src/gamelab/TextConsole.jsx
+++ apps/src/gamelab/TextConsole.jsx
@@ -21,16 +21,52 @@ export const styles = {
   hide: {
     display: 'none'
   },
-  console: {
+  consoleOpen: {
     display: '',
     background: 'rgba(128,128,128,0.3)',
-    maxHeight: '23%',
+    maxHeight: '19%',
     position: 'absolute',
     top: 0,
     left: 0,
     zIndex: 2,
     width: '100%',
     overflow: 'auto'
+  },
+  consoleClosed: {
+    display: '',
+    background: 'rgba(128,128,128,0.3)',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 2,
+    width: '100%',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+  },
+  expandButton: {
+    position: 'absolute',
+    right: '0',
+    zIndex: 3,
+    minWidth: '16px',
+    margin: '0px',
+    border: '0px',
+    padding: '0px',
+    fontSize: 'inherit',
+    lineHeight: 'inherit',
+    borderRadius: '0px',
+    fontFamily: 'monospace'
+  },
+  collapseButton: {
+    position: 'absolute',
+    right: '0',
+    zIndex: 3,
+    minWidth: '30px',
+    margin: '0px',
+    border: '0px',
+    padding: '0px',
+    fontSize: 'inherit',
+    lineHeight: 'inherit',
+    borderRadius: '0px'
   }
 };
 
@@ -47,12 +83,33 @@ export default class TextConsole extends React.Component {
     show: PropTypes.bool.isRequired
   };
 
+  state = {
+    closed: true
+  };
+
+  getConsoleStyle() {
+    return this.state.closed? styles.consoleClosed : styles.consoleOpen;
+  }
+
+  toggleStyle() {
+    this.setState((state) => {
+      return {closed: !state.closed};
+    });
+  }
+
+  getButtonText() {
+    return this.state.closed? '+' : '-';
+  }
+
   render() {
     return (
-      <div id="text-console" className="text-console" style={styles.console} >
-        <text x={5} y={15}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </text>
+      <div>
+        <span id="text-console" className="text-console" style={this.getConsoleStyle()} >
+          <text x={5} y={15}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </text>
+        </span>
+        <button type="button" id="expand-collapse" style={styles.expandButton} onClick={() => this.toggleStyle()}>{this.getButtonText()}</button>
       </div>
     );
   }
